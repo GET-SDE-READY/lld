b6ed375d910f747ae0bebbe99737b5bd7306951a
Vinamer <84142017+Vinamer@users.noreply.github.com>
2024-01-12 20:30:12 +0530

    Add files via upload

---
+++ design-patterns/BridgeDemo.java
@@ -0,0 +1,62 @@
+package com.amazon.awdstoragefeemanager.exception;
+
+public class BridgeDemo {
+    public static void main(String[] args) {
+        Remote remote = new Remote(new TV());
+        remote.togglePower();
+        remote.togglePower();
+    }
+}
+
+interface Device {
+    boolean isOn();
+    void turnOn();
+    void turnOff();
+
+}
+
+class TV implements Device {
+
+    private boolean isOn = false;
+
+    @Override
+    public boolean isOn() {
+        if (isOn)
+            System.out.println("TV is on");
+        else
+            System.out.println("TV is off");
+        return isOn;
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Turning TV On");
+        isOn = true;
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Turning TV Off");
+        isOn = false;
+    }
+}
+
+class Remote {
+
+    private Device device;
+
+    public Remote(Device device) {
+        this.device = device;
+    }
+
+    public void togglePower() {
+        if (this.device.isOn()) {
+            this.device.turnOff();
+        } else {
+            this.device.turnOn();
+        }
+    }
+}
+
+
+
